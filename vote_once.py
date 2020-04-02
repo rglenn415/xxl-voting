ad57f903176272997e0eed7e2a4ff9f99241f9d1
rglenn415 <ryanglenn415@gmail.com>
2020-04-02 08:59:00 -0700

    adding both files

---
+++ vote_once.py
@@ -0,0 +1,5 @@
+import requests
+headers = {'User-Agent':"Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"}
+data = {"action": "media_poll_vote", "poll_id": "10523173", "post_id": 884664, "answer_id": "48703337"}
+res = requests.post('https://www.xxlmag.com/rest/carbon/api/poll/votenow', data = data, headers=headers)
+print(res.json())
